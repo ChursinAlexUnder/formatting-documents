c2a1dba9045f13a2fc07c5ba06ca662b3fabfa00
ChursinAlexUnder <keks63620@gmail.com>
2024-09-04 00:03:54 +0300

    Create Python scrypt for taking text form document

--- golang/filework/filework.go
+++ golang/filework/filework.go
@@ -2,26 +2,35 @@ package filework
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"text/template"
 )
 
 type Answer struct {
-	FileName string
-	FileSize int64
-	Change   string
+	FileName, Text, Change string
 }
 
 func FormSend(w http.ResponseWriter, r *http.Request) {
 	// получение значения полей
 	var change string
-	file, filedata, err := r.FormFile("document-file")
+	file, fileData, err := r.FormFile("document-file")
 	if err != nil {
 		fmt.Fprintf(w, "Error: %v", err)
 		return
 	}
 	defer file.Close()
 	change = r.FormValue("change")
+
+	// Проверка расширения файла на docx
+	if ext := strings.ToLower(filepath.Ext(fileData.Filename)); ext != ".docx" {
+		http.Error(w, "Invalid file extension", http.StatusUnsupportedMediaType)
+		return
+	}
 
 	// загрузка и распарсивание HTML-шаблона из файла temp.html для дальнейшей работы
 	tmp, err := template.ParseFiles("./html/temp.html")
@@ -30,7 +39,44 @@ func FormSend(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Сохранение файла на диск
+	tempFile, err := os.CreateTemp("", "temp-*.docx")
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v", err)
+		return
+	}
+	// выполняться будут в обратном порядке
+	defer os.Remove(tempFile.Name()) // Удаление файла после использования
+	defer tempFile.Close()
+
+	_, err = io.Copy(tempFile, file)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v", err)
+		return
+	}
+
+	// Вызов Python-скрипта для извлечения текста
+	text, err := textFromDocx(tempFile.Name())
+	if err != nil {
+		fmt.Fprintf(w, "Error11111111: %v", err)
+		return
+	}
+
 	// добавление данных на страницу
-	answer := Answer{filedata.Filename, filedata.Size, change}
+	answer := Answer{fileData.Filename, text, change}
 	tmp.Execute(w, answer)
+}
+
+func textFromDocx(filename string) (string, error) {
+	// Вызов Python-скрипта
+	cmd := exec.Command("python3", "github.com/ChursinAlexUnder/Golang-website/python/editDocument.py", filename)
+
+	// Получение вывода из Python-скрипта
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	// Возврат текста как строки
+	return strings.TrimSpace(string(output)), nil
 }
