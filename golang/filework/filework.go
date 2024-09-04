fb259f4b29819f2212b69e71c1501bf8c9cfc196
ChursinAlexUnder <keks63620@gmail.com>
2024-09-04 19:57:29 +0300

    some fix in css

--- golang/filework/filework.go
+++ golang/filework/filework.go
@@ -2,17 +2,14 @@ package filework
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 	"text/template"
 )
 
 type Answer struct {
-	FileName, Text, Change string
+	FileName, Change string
 }
 
 func FormSend(w http.ResponseWriter, r *http.Request) {
@@ -39,44 +36,7 @@ func FormSend(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Сохранение файла на диск
-	tempFile, err := os.CreateTemp("", "temp-*.docx")
-	if err != nil {
-		fmt.Fprintf(w, "Error: %v", err)
-		return
-	}
-	// выполняться будут в обратном порядке
-	defer os.Remove(tempFile.Name()) // Удаление файла после использования
-	defer tempFile.Close()
-
-	_, err = io.Copy(tempFile, file)
-	if err != nil {
-		fmt.Fprintf(w, "Error: %v", err)
-		return
-	}
-
-	// Вызов Python-скрипта для извлечения текста
-	text, err := textFromDocx(tempFile.Name())
-	if err != nil {
-		fmt.Fprintf(w, "Error11111111: %v", err)
-		return
-	}
-
 	// добавление данных на страницу
-	answer := Answer{fileData.Filename, text, change}
+	answer := Answer{fileData.Filename, change}
 	tmp.Execute(w, answer)
-}
-
-func textFromDocx(filename string) (string, error) {
-	// Вызов Python-скрипта
-	cmd := exec.Command("python3", "../../python/edit-document.py", filename)
-
-	// Получение вывода из Python-скрипта
-	output, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
-
-	// Возврат текста как строки
-	return strings.TrimSpace(string(output)), nil
 }
