5d59c5cc404ba911d4905f436d789ec43856d385
ChursinAlexUnder <keks63620@gmail.com>
2024-10-17 00:14:45 +0300

    awd

--- main.go
+++ main.go
@@ -28,7 +28,6 @@ func handleRequest() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/formData", filework.FormSend)
 	http.HandleFunc("/download", filework.DownloadFile)
-	http.HandleFunc("/api/", filework.PythonProxy)
 	// Проксируем запросы на Python-сервис (например, для обработки файлов)
 	http.ListenAndServe(":8080", nil)
 }
