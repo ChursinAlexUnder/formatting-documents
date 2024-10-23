72eab2956f7538ee94cf5be8001d5bd56b267934
ChursinAlexUnder <keks63620@gmail.com>
2024-10-24 00:54:09 +0300

    Update downloader.go

--- internal/infrastructure/downloader.go
+++ internal/infrastructure/downloader.go
@@ -9,13 +9,13 @@ import (
 // сохранение файла на сервере в папке buffer
 func SaveDocument(data domain.Answer) error {
 	var (
-		bufferPath string = "../buffer/" + data.DocumentData.Filename
+	// bufferPath string = "../buffer/" + data.DocumentData.Filename
 	)
 	// downloadDocument, err := os.Create(bufferPath)
 	// if err != nil {
 	// 	return fmt.Errorf("error creating new empty document: %v", err)
 	// }
-	if _, err := os.Stat(bufferPath); err != nil {
+	if _, err := os.Stat("../buffer/"); err != nil {
 		return fmt.Errorf("error: %v", err)
 	}
 	// defer downloadDocument.Close()
