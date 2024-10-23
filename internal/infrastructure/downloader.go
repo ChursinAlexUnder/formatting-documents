7d6b4f661fdcfa23e4eff964c5d5ab9e7815521c
ChursinAlexUnder <keks63620@gmail.com>
2024-10-24 01:00:51 +0300

    123

--- internal/infrastructure/downloader.go
+++ internal/infrastructure/downloader.go
@@ -3,26 +3,24 @@ package infrastructure
 import (
 	"fmt"
 	"formatting-documents/internal/domain"
+	"io"
 	"os"
 )
 
 // сохранение файла на сервере в папке buffer
 func SaveDocument(data domain.Answer) error {
 	var (
-	// bufferPath string = "../buffer/" + data.DocumentData.Filename
+		bufferPath string = "../buffer/" + data.DocumentData.Filename
 	)
-	// downloadDocument, err := os.Create(bufferPath)
-	// if err != nil {
-	// 	return fmt.Errorf("error creating new empty document: %v", err)
-	// }
-	if _, err := os.Stat("../buffer/"); err != nil {
-		return fmt.Errorf("error: %v", err)
+	downloadDocument, err := os.Create(bufferPath)
+	if err != nil {
+		return fmt.Errorf("error creating new empty document: %v", err)
 	}
-	// defer downloadDocument.Close()
+	defer downloadDocument.Close()
 
-	// _, err = io.Copy(downloadDocument, data.Document)
-	// if err != nil {
-	// 	return fmt.Errorf("error writing new emptry document: %v", err)
-	// }
+	_, err = io.Copy(downloadDocument, data.Document)
+	if err != nil {
+		return fmt.Errorf("error writing new emptry document: %v", err)
+	}
 	return nil
 }
