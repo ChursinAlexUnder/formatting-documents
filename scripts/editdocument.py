79576353d694a7857ae312c69120d831d5fd41cf
ChursinAlexUnder <keks63620@gmail.com>
2024-11-11 21:03:11 +0300

    Added some function for Python-script

--- scripts/editdocument.py
+++ scripts/editdocument.py
@@ -1,17 +1,26 @@
 import sys
 import os
 from docx import Document
+from docx.shared import Pt
 
 def formatDocument(bufferPath, documentName, comment):
     # Открываем документ
     doc = Document(bufferPath + '/' + documentName)
+
+    # обработка всего документа (по всем paragraphs и всем runs)
+    for paragraph in doc.paragraphs:
+        for run in paragraph.runs:
+            run.font.size = Pt(14)
+            run.font.name = 'Times New Roman'
 
     # Добавляем текст комментария в конец документа
     if comment:
         doc.add_paragraph(comment)
     else:
         doc.add_paragraph('Комментарий отсутствует, так что просто хорошего дня)')
+    
 
+    # Работа с именем отформатированного документа
     formattedDocumentName = 'formatted_' + documentName
     formattedDocumentPath = bufferPath + '/' + formattedDocumentName
     doc.save(formattedDocumentPath)
