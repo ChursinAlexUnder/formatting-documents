82bb8baf907a259a01cac47b0deb2313310a0d51
ChursinAlexUnder <keks63620@gmail.com>
2025-03-05 00:17:59 +0300

    Update JSON.go

--- internal/services/JSON.go
+++ internal/services/JSON.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"formatting-documents/internal/domain"
-	"os"
+	"io/ioutil"
 	"sync"
 	"time"
 )
@@ -13,38 +13,44 @@ var mu sync.Mutex
 
 func CheckDataJSON() error {
 	var (
-		filename string = "data.json"
-		data     domain.Data
+		filename string      = "data.json" // Имя файла
+		data     domain.Data               // Переменная для данных
 	)
-	mu.Lock()
-	defer mu.Unlock()
+	mu.Lock()         // Захватываем мьютекс
+	defer mu.Unlock() // Освобождаем мьютекс после выхода из функции
 
-	currentDate := time.Now().Format("2006-01-02")
+	currentDate := time.Now().Format("2006-01-02") // Получаем текущую дату
 
-	file, err := os.ReadFile(filename)
+	// Чтение файла через ioutil
+	file, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return fmt.Errorf("error reading json file: %v", err)
 	}
 
+	// Декодируем JSON в структуру
 	err = json.Unmarshal(file, &data)
 	if err != nil {
 		return fmt.Errorf("error decoding json file: %v", err)
 	}
 
+	// Если дата устарела — обнуляем данные
 	if data.Date != currentDate {
 		data.Count = 0
 		data.Date = currentDate
 		data.LastFormatting = []domain.Parameters{}
 
+		// Кодируем данные обратно в JSON
 		updatedFile, err := json.MarshalIndent(data, "", "    ")
 		if err != nil {
 			return fmt.Errorf("error coding json: %v", err)
 		}
 
-		err = os.WriteFile(filename, updatedFile, 0644)
+		// Записываем файл через ioutil
+		err = ioutil.WriteFile(filename, updatedFile, 0644)
 		if err != nil {
 			return fmt.Errorf("error writing in json file: %v", err)
 		}
+		fmt.Println("JSON file updated successfully")
 	}
 	return nil
 }
